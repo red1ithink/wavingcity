04e217c93e37b8a3c825863d69d4ef73f174096f
red1ithink <89466710+red1ithink@users.noreply.github.com>
2024-03-20 20:00:21 +0900

    Delete wavwav2 directory

--- wavwav2/part_travel/red1ithink.travel.france.github.io/France/france.js
+++
@@ -1,9 +0,0 @@
-const box = document.querySelector('.box');
-
-window.addEventListener('scroll', () => {
-  const boxPosition = box.getBoundingClientRect().top;
-
-  if (boxPosition < window.innerHeight / 1.5) {
-    box.style.opacity = 1;
-  }
-});
